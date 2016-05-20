1fda04f90f0107517f15a32be368522fda6d07ba
John Caraballo <jcaraballo019@gmail.com>
2016-05-19 22:03:12 -0400

    Removed only http protocol requirement for routes

--- server.js
+++ server.js
@@ -4,14 +4,6 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
-  if (req.protocol === 'http'){
-    next();
-  } else {
-    res.redirect('http://' + req.hostname + req.url);
-  }
-});
-
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
